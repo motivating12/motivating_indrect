df2f25b3967460bc9c79387502f892999b252dba
motivating12 <155353359+motivating12@users.noreply.github.com>
2024-01-02 15:45:03 +0800

    Add files via upload

--- motivating_ex.c
+++ motivating_ex.c
@@ -8,26 +8,17 @@ char *copy(char *dest, const char* src){
 	return strcpy(dest, src);
 }
 
-
-
-void op(char *(str_op)(char *, const char *), const char *data){
-    char sensitive_data[20];
-	(*str_op)(sensitive_data, data); 
-    char dest[50];
-	memcpy(dest,sizeof(sensitive_data),sensitive_data);
-}
-
-int auth(const char *buf){
-	if( !strcmp(buf, "passwd") ){
+void auth(){
+		char (*str_op)(char *, const char *) = copy;
 		char data[100];
-		fgets(data,sizeof(data),stdin);
-		op(copy, data); //SLIVER_SOURCE
-		return 1;
-	}
-	return 0;
+		fgets(data,sizeof(data),stdin);//SLIVER_SOURCE
+		char sensitive_data[20];
+		(*str_op)(sensitive_data, data); 
+	    	char dest[50];
+		memcpy(dest,sensitive_data,sizeof(sensitive_data));		
 }
 
-void main(int argc, char **argv){
-	int auth_ret=auth(argv[1]);
+void main(){
+	auth();
 	
 }
